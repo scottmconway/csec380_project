9fee91c03e12b4dda605f2408355d9ae2fdec956
Nathaniel Beckstead <becksteadn@gmail.com>
2018-10-13 14:51:10 -0400

    Docker container and server running for Java auth

---
+++ Authentication/README.md
@@ -0,0 +1,5 @@
+# Authentication Server - Java
+
+Basic setup using [Spring Security](https://spring.io/guides/gs/authenticating-ldap/). 
+
+Communicates with RIT LDAP over TLS.
---
+++ Authentication/auth_server/src/main/java/auth/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().fullyAuthenticated()
+				.and()
+			.formLogin();
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.ldapAuthentication()
+				.userDnPatterns("uid={0},ou=people")
+				.groupSearchBase("ou=groups")
+				.contextSource()
+					.url("ldaps://ldap.rit.edu:636/dc=rit,dc=edu")
+					.and()
+				.passwordCompare()
+					.passwordEncoder(new LdapShaPasswordEncoder())
+					.passwordAttribute("userPassword");
+	}
+
+}
