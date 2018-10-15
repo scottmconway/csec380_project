924868f79c2036c3a887a08ff523c7be6151296c
Nathaniel Beckstead <becksteadn@gmail.com>
2018-10-15 12:56:27 -0400

    started Authentication tests

---
+++ Authentication/tests/test.py
@@ -0,0 +1,11 @@
+#
+# Description: Test that a user is able to login with RIT LDAP credentials
+# 
+#
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.add_argument('-headless')
+firefox = Firefox(firefox_options=options)
+firefox.get('http://localhost:8080')
