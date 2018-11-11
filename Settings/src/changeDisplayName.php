17d13b11d0ca10a44bee2454142566e80d0acdee
Scott Conway <scottmconway@users.noreply.github.com>
2018-11-11 16:12:26 -0500

    Added prototype for /changeDisplayName script

---
+++ Settings/src/changeDisplayName.php
@@ -0,0 +1,23 @@
+<?php
+$auth_token = $_SERVER['HTTP_authtoken'];
+
+$user_id = http_get("/isAuthenticated", array("headers"=>$auth_token));
+
+if($user_id == "INVALID SESSION"){
+		die("Invalid Session");
+	}
+	
+	if(!isset($_GET['new_name'])){
+        die("new_name is not set")
+    }
+
+    $conn = new mysqli("127.0.0.1", "root", "skitteradmin", "skitter");
+        // Check connection
+        if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+        }
+
+        $stmt = $conn->prepare("UPDATE user SET (display_name) VALUES (?) WHERE user_id=?");
+        $stmt->bind_param("si", $new_name, $user_id);
+        $stmt->execute();
+?>	
